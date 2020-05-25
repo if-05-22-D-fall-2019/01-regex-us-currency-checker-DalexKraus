f85c2e42d603f21c0aab5e88530c7a027012ab9e
DalexKraus <dalex.kraus@gmail.com>
2020-05-25 08:35:07 +0200

    One missing

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
 
-    private static final String CURRENCY_PATTERN = "\\$\\s\\d+(.\\d+)?";
+    private static final String CURRENCY_PATTERN = "\\$\\s?\\d+(.\\d{1,2})?";
     private static final Pattern currencyPattern = Pattern.compile(CURRENCY_PATTERN);
 
     public static Matcher getMatcher(String usCurrencyString) {
